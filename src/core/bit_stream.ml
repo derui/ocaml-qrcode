50fcf146c7f04c3d9a1f5c49aa943387e7279afb
derui <derutakayu@gmail.com>
2022-04-26 21:34:41 +0900

    feat: add dune

---
+++ src/core/bit_stream.ml
@@ -0,0 +1,6 @@
+(** This module provides stream of bit. *)
+
+(** A data type give data to a sequence *)
+type bit =
+  | One
+  | Zero
--- src/core/type.ml
+++ src/core/type.ml
@@ -83,5 +83,6 @@ module Segment = struct
   type t = {
     mode : Mode.t;
     count_indicator : Count_indicator.t;
+    data : bytes;
   }
 end
