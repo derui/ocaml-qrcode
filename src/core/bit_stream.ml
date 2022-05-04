1cd926190a4fd871054679f212192cebf6a018bb
derui <derutakayu@gmail.com>
2022-05-04 13:27:10 +0900

    feat: add function implementation

--- src/core/bit_stream.ml
+++ src/core/bit_stream.ml
@@ -1,6 +1,7 @@
 (** This module provides stream of bit. *)
 
 type t = {
+  mutable current_bits : int;
   mutable bits : int;
   mutable buffer : Stdint.Uint128.t array;
 }
@@ -9,7 +10,11 @@ type bit =
   | One
   | Zero
 
-let create () = { bits = 0; buffer = Array.make 1 Stdint.Uint128.zero }
+type bit_result =
+  | Continue of bit
+  | Eos
+
+let create () = { current_bits = 0; bits = 0; buffer = Array.make 1 Stdint.Uint128.zero }
 
 let make_bitmask index =
   let shift = pred (Stdint.Uint128.bits - index) in
@@ -35,7 +40,16 @@ let put_data data t =
     let target_bit = make_bitmask reminder in
     let index = if reminder > 0 then next_bits / Stdint.Uint128.bits else succ (next_bits / Stdint.Uint128.bits) in
     t.buffer.(index) <- Stdint.Uint128.logor t.buffer.(index) target_bit;
-    { buffer = t.buffer; bits = next_bits })
+    { t with buffer = t.buffer; bits = next_bits })
   else { t with bits = next_bits }
 
 let put ~data:bit t = match bit with One -> put_data Stdint.Uint128.one t | Zero -> put_data Stdint.Uint128.zero t
+
+let next t =
+  if t.current_bits >= t.bits then Eos
+  else (
+    t.current_bits <- succ t.current_bits;
+    let index = t.current_bits / Stdint.Uint128.bits and reminder = t.current_bits mod Stdint.Uint128.bits in
+    let bit_mask = make_bitmask reminder in
+    let value = Stdint.Uint128.(logand t.buffer.(index) bit_mask) in
+    if Stdint.Uint128.(compare zero value) = 0 then Continue Zero else Continue One)
