e672a572bbce1eb4f9342abae9373dabd4010ade
derui <derutakayu@gmail.com>
2022-05-09 20:59:20 +0900

    refactor: merge encoder

--- src/core/segment.mli
+++ src/core/segment.mli
@@ -6,12 +6,15 @@ type t = {
 
 type encoding_error =
   | Invalid_data of string
-  | Data_size_overflow of string * int
+  | Data_size_overflow of string * int  (** encoding error *)
 
 type encoded = (t, encoding_error) result
+(** type of encoding result *)
 
 type data_generator = unit -> char option
+(** type of data generator. Return None if no data *)
 
 module type S = sig
   val encode : metadata:Metadata.t -> generator:data_generator -> encoded
+  (** [encode ~metadata ~generator] get a segment from data generated from generator and metadata *)
 end
