4116f1d66ff634c4c8a1a4dff0a36140b431e114
derui <derutakayu@gmail.com>
2022-05-28 13:35:29 +0900

    feat: add support function for array

--- src/core/std.ml
+++ src/core/std.ml
@@ -5,3 +5,19 @@ module Result = struct
     let ( let* ) = Result.bind
   end
 end
+
+module Array = struct
+  include Stdlib.Array
+
+  let loop ?(mode = `Exclusive) ~range ~f t =
+    let from, to_ = range in
+    let from, to_ = if mode = `Exclusive then (from, pred to_) else (from, to_) in
+
+    let rec loop' current =
+      if current > to_ then ()
+      else (
+        f current t.(current - from);
+        loop' (succ current))
+    in
+    loop' from
+end
